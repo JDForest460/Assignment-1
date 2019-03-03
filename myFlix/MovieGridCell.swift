0c7932a073b706caf4f05dbdd577702a3b5b280f
Jiade LIn <jdforest460@gmail.com>
2019-03-03 15:49:35 -0500

    finish assignment#2

---
+++ myFlix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  myFlix
+//
+//  Created by Jiade Lin on 3/3/19.
+//  Copyright © 2019 Jiade Lin. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
